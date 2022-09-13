ec8028b5ae8371004401b52ee866defeec65d66a
DoCE <briancho325@gmail.com>
2022-09-13 09:49:18 +0900

    0912

---
+++ winter_22/src/winter_22/Grading.java
@@ -0,0 +1,31 @@
+package winter_22;
+
+import java.util.Scanner; //이게 필수네
+
+public class Grading {
+
+	public static void main(String[] args) {
+		char grade;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("점수를 입력하세요(0~100): ");
+		int score = scanner.nextInt(); //점수 읽기
+		if(score >= 90)
+			grade = 'A';
+		else if (score >= 80)
+			grade = 'B';
+		else if (score >= 70)
+			grade = 'C';
+		else if (score >= 60)
+			grade = 'D';
+		else
+			grade = 'F';
+		
+		System.out.println("학점은 "+ grade + "입니다");
+		
+		scanner.close();
+		
+		
+	}
+
+}
